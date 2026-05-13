161c8ca2d9f30391a2bd3cf17c22496c6baa5fc4
Pavel Doležal <pajmacz@gmail.com>
2026-05-13 20:00:16 +0000

    2. stránka

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 	
 	if request.method == "POST":
 		heslo=request.form.get("heslo")
-		spravne_heslo="Pavel Doležal"
+		spravne_heslo="Pavel Doležal"or "pavel doležal"
 		if heslo==spravne_heslo:
 			odpoved="správná"
 			session["pocet_pokusu"] = 0
@@ -29,6 +29,32 @@ def index():
 			pocet_pokusu += 1
 			session["pocet_pokusu"] = pocet_pokusu
 	return render_template("uvodni.html", odpoved=odpoved, pocet_pokusu=pocet_pokusu)
+
+@app.route("/sýr", methods=["GET", "POST"])
+def syr():
+	odpoved = None
+	current_route = request.path
+	last_route = session.get("last_route", None)
+	
+	# Pokud se změní stránka, resetuj počet pokusů
+	if last_route != current_route:
+		pocet_pokusu = 0
+		session["pocet_pokusu"] = 0
+		session["last_route"] = current_route
+	else:
+		pocet_pokusu = session.get("pocet_pokusu", 0)
+	
+	if request.method == "POST":
+		heslo=request.form.get("heslo")
+		spravne_heslo="knír"or "Knír"
+		if heslo==spravne_heslo:
+			odpoved="správná"
+			session["pocet_pokusu"] = 0
+		else:
+			odpoved="špatná"
+			pocet_pokusu += 1
+			session["pocet_pokusu"] = pocet_pokusu
+	return render_template("syr.html", odpoved=odpoved, pocet_pokusu=pocet_pokusu)
 
 if __name__=="__main__":
 	app.run(debug=True)
